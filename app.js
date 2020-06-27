75a199021c16cecae4103d7a87e59f9008b214b3
Christoper Smith <Christophersmithcode@gmail.com>
2020-06-26 22:10:36 -0400

    working on it

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("connected and running");
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var gold = [255, 215, 0]
+
+var myName = "Christopher Smith";
+letterColors = [gold, gold, green, gold, gold];
+if(15 >5) {
+    bubbleShape = "circle";
+    }
+    else {
+        bubbleShape = "square";
+    }
+
+
+drawName(myName, letterColors, bubbleShape);
+bounceBubbles();
